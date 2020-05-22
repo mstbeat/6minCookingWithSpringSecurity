591f34040ae4a2cfa92b95af3be0a7e490ac097a
mstbeat <mrbrightside82@ymail.com>
2020-05-22 18:38:08 +0900

    fix for review

--- src/main/java/cooking/repository/ProductMapper.java
+++ src/main/java/cooking/repository/ProductMapper.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) Proud Data Co., Ltd. All Rights Reserved.
+ * Please read the associated COPYRIGHTS file for more details. *
+ * THE SOFTWARE IS PROVIDED BY Proud Group
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * CLAIM, DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. */
+
 package cooking.repository;
 
 import java.sql.Timestamp;
@@ -8,16 +19,44 @@ import org.apache.ibatis.annotations.Param;
 
 import cooking.entity.Product;
 
+/**
+ * 商品情報のマッパー.
+ * @author Masato Yasuda
+ */
 @Mapper
 public interface ProductMapper {
 
+	/**
+	 * 全ての商品情報を取得するメソッド
+	 * @return 全ての商品情報
+	 */
 	List<Product> getProductInfoList();
 
+	/**
+	 * 商品情報を登録するメソッド
+	 * @param product 商品情報
+	 */
 	void createProduct(Product product);
 
+	/**
+	 * 商品IDで商品情報を取得するメソッド
+	 * @param productID 商品ID
+	 * @return 商品情報
+	 */
 	Product findById(int productID);
 
+	/**
+	 * 商品情報を更新するメソッド
+	 * @param product 商品情報
+	 * @return 商品更新件数
+	 */
 	int updateProduct(Product product);
 
+	/**
+	 * 商品情報を削除するメソッド
+	 * @param productID 商品ID
+	 * @param updateDate 更新日時
+	 * @return 商品削除件数
+	 */
 	int deleteProduct(@Param("productID") int productID, @Param("updateDate") Timestamp updateDate);
 }
--- src/main/java/cooking/validator/FileSize.java
+++ src/main/java/cooking/validator/FileSize.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) Proud Data Co., Ltd. All Rights Reserved.
+ * Please read the associated COPYRIGHTS file for more details. *
+ * THE SOFTWARE IS PROVIDED BY Proud Group
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * CLAIM, DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+ * OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. */
+
 package cooking.validator;
 
 import java.lang.annotation.Documented;
@@ -9,12 +20,19 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-@Target({ElementType.TYPE,ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER})
+/**
+ * 画像ファイルサイズのバリデーションのアノテーション設定.
+ * @author Masato Yasuda
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
+		ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy=FileSizeValidator.class)
+@Constraint(validatedBy = FileSizeValidator.class)
 @Documented
 public @interface FileSize {
 	String message();
+
 	Class<?>[] groups() default {};
+
 	Class<? extends Payload>[] payload() default {};
 }
