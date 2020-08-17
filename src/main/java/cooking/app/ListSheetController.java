0ae8f04dbe1f47b572d339b13fc817dd89dc3063
mstbeat <mrbrightside82@ymail.com>
2020-08-17 18:41:16 +0900

    modify jrxml files

--- src/main/java/cooking/app/ListSheetController.java
+++ src/main/java/cooking/app/ListSheetController.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import cooking.entity.Product;
 import cooking.service.ProductService;
@@ -35,24 +38,26 @@ public class ListSheetController {
 	ProductService productService;
 	
 	@GetMapping("/list-sheet")
-	public String createSheet(HttpServletResponse response) {
+	public String createListSheet(HttpServletResponse response) {
 		/**▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼　データ作成部　▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼**/
         //ヘッダーデータ作成
         HashMap<String, Object> params = new HashMap<String, Object>();
         params.put("title", "商品情報一覧");
+        params.put("no_image", "../resources/static/images/no_image.png");
 
         //フィールドデータ作成
         List<Product> fields = productService.findAll();
+        params.put("count", fields.size());
 
         /**▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲　データ作成部　▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲**/
         /**▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼　帳票出力部　▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼**/
         //データを検索し帳票を出力
-        byte[] output  = OrderReport(params, fields);
+        byte[] output  = orderListReport(params, fields);
         /**▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲　データ作成部　▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲**/
 
         /**▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼　データ作成データダウンロード部 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼**/
         response.setContentType("application/octet-stream");
-        response.setHeader("Content-Disposition", "attachment; filename=" + "sample.pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=" + "product-list.pdf");
         response.setContentLength(output.length);
 
         OutputStream os = null;
@@ -71,6 +76,44 @@ public class ListSheetController {
         return null;
     }
 	
+	@PostMapping("/update-sheet")
+	public String createUpdateSheet(HttpServletResponse response, @ModelAttribute("productID") int productID) {
+		/**▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼　データ作成部　▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼**/
+        //ヘッダーデータ作成
+        HashMap<String, Object> params = new HashMap<String, Object>();
+        params.put("title", "商品情報更新");
+        params.put("no_image", "/cooking/src/main/resources/static/images/no_image.png");
+
+        //フィールドデータ作成
+        List<Product> fields = new ArrayList<Product>();
+        fields.add(productService.findOne(productID));
+
+        /**▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲　データ作成部　▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲**/
+        /**▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼　帳票出力部　▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼**/
+        //データを検索し帳票を出力
+        byte[] output  = orderUpdateReport(params, fields);
+        /**▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲　データ作成部　▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲**/
+
+        /**▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼　データ作成データダウンロード部 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼**/
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=" + "product-update.pdf");
+        response.setContentLength(output.length);
+
+        OutputStream os = null;
+        try {
+            os = response.getOutputStream();
+            os.write(output);
+            os.flush();
+
+            os.close();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+        /**▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲　データ作成部　▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲**/
+
+
+        return null;
+    }
 	
 	/**
 	 * ジャスパーレポートコンパイル。バイナリファイルを返却する。
@@ -78,11 +121,50 @@ public class ListSheetController {
 	 * @param response
 	 * @return
 	 */
-	private byte[] OrderReport(HashMap<String, Object> param, List<Product> data) {
+	private byte[] orderListReport(HashMap<String, Object> param, List<Product> data) {
 		InputStream input;
 		try {
 			//帳票ファイルを取得
 			input = new FileInputStream(resource.getResource("classpath:reports/product-list2.jrxml").getFile());
+			//リストをフィールドのデータソースに
+			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(data);
+			//帳票をコンパイル
+			JasperReport jasperReport = JasperCompileManager.compileReport(input);
+
+			JasperPrint jasperPrint;
+			//パラメーターとフィールドデータを注入
+			jasperPrint = JasperFillManager.fillReport(jasperReport, param, dataSource);
+			//帳票をByte形式で出力
+			return JasperExportManager.exportReportToPdf(jasperPrint);
+
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (JRException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+	
+	/**
+	 * ジャスパーレポートコンパイル。バイナリファイルを返却する。
+	 * @param data
+	 * @param response
+	 * @return
+	 */
+	private byte[] orderUpdateReport(HashMap<String, Object> param, List<Product> data) {
+		InputStream input;
+		try {
+			//帳票ファイルを取得
+			input = new FileInputStream(resource.getResource("classpath:reports/product-update.jrxml").getFile());
 			//リストをフィールドのデータソースに
 			JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(data);
 			//帳票をコンパイル
